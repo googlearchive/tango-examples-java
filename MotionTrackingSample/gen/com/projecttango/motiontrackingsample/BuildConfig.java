9d09ea2d3425e352aa8cb2c67ae1348657b7d1a9
r4ravi2008 <r4ravi2008@gmail.com>
2014-07-23 10:58:56 -0700

    Emptying Master

--- MotionTrackingSample/gen/com/projecttango/motiontrackingsample/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.projecttango.motiontrackingsample;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
