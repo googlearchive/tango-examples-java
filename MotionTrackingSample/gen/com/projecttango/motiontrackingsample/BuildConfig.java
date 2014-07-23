7ffc5121cb5efc13e7aa3b572b14992ac238f31f
r4ravi2008 <r4ravi2008@gmail.com>
2014-07-23 11:00:33 -0700

    Motion Tracking Sample

---
+++ MotionTrackingSample/gen/com/projecttango/motiontrackingsample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.projecttango.motiontrackingsample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
